6eaf34189b734cdc7e130d1d66cd883cc1e53728
Edward Leon <edleon761@gmail.com>
2025-02-07 18:41:02 -0800

    Brought in TMP and made a button

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class ExitButton : MonoBehaviour
+{
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartTheGame()
+    {
+        // Leads into main game scene
+    }
+    
+    
+}
